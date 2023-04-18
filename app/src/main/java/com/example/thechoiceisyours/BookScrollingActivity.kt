22d2843e2bb7229a5127969ede69cda57e67f101
Tamsters24 <102896617+Tamsters24@users.noreply.github.com>
2023-04-17 22:02:09 -0700

    Working on BookScrollingActivity.kt. Reformatted to introduce multiple choices, but bug when 3 choices.

--- app/src/main/java/com/example/thechoiceisyours/BookScrollingActivity.kt
+++ app/src/main/java/com/example/thechoiceisyours/BookScrollingActivity.kt
@@ -1,16 +1,12 @@
 package com.example.thechoiceisyours
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Typeface
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
-import android.text.Layout
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.StyleSpan
-import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -20,11 +16,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
 import com.example.thechoiceisyours.databinding.ActivityBookScrollingBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
@@ -44,15 +36,16 @@ class BookScrollingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Menu options
         binding = ActivityBookScrollingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(findViewById(R.id.toolbar))
         binding.toolbarLayout.title = title
 
         // Retrieve Book Contents using the Extra put in BookCover.kt
         bookAssets = intent.getStringExtra("assetsFolder").toString()
-        // Concatenate name for direct43ory and book content .txt file
+
+        // Concatenate name for directory and book content .txt files
         assetsDirectory = bookAssets + "_files/"
         val bookFile = bookAssets + "_contents.txt"
         val nextChoicesFile = bookAssets + "_nextChoices.txt"
@@ -60,6 +53,7 @@ class BookScrollingActivity : AppCompatActivity() {
         val nextChoicesContents = assetsDirectory + nextChoicesFile
         nodesVisitedDB = bookAssets + "NodesVisited"
 
+        // Open the book, set the display, and update the visited chapters when accessed
         getStory(bookContents)
         choiceMap = setChoiceMap(nextChoicesContents)
         storyDisplay()
@@ -83,20 +77,21 @@ class BookScrollingActivity : AppCompatActivity() {
     private fun storyDisplay() {
         Toast.makeText(baseContext, "Part $currentPart", Toast.LENGTH_SHORT).show()
 
-        // Access the Firebase database and toggle the current Part.Choice "visited" to true
+        // Access the Firebase database for Story Progress graph
+        // Toggled the current Part.Choice "visited" to true when accessed
         val firebaseAuth = FirebaseAuth.getInstance()
         val user = firebaseAuth.currentUser
         val userId = user?.uid.toString()
         val database = FirebaseDatabase.getInstance()
         val usersRef = database.getReference("users")
         val userRef = usersRef.child(userId)
-        val visitedChapter = userRef.child(nodesVisitedDB).
-                             child("$currentPart$currentChoice")
-        if (currentPart > 1)  // First chapter is always considered visited
+        if (currentPart > 1) { // First chapter is always considered visited
+            val visitedChapter = userRef.child(nodesVisitedDB).
+            child("$currentPart$currentChoice")
             visitedChapter.setValue(true)
+        }
 
-
-        // Display the current chapter image if one exists
+        // Update the current chapter image if one exists
         val filteredImage = storyLines.filter { it.contains("p$currentPart$currentChoice") }
         if (filteredImage.isNotEmpty()) {
             var chapterImageString = filteredImage[0]
@@ -109,9 +104,11 @@ class BookScrollingActivity : AppCompatActivity() {
 
         // Adjust the Choice Buttons display according to the number of choices available
         val choices = getNextChoices("$currentPart$currentChoice")
+        for (choice in choices)
+            Toast.makeText(baseContext, choice, Toast.LENGTH_SHORT).show()
         displayButtons(choices)
 
-        currentPart += 1
+        currentPart += 1  // Increment to the next chapter
     }
 
     // Set image for current chapter
@@ -120,9 +117,6 @@ class BookScrollingActivity : AppCompatActivity() {
         val image = Drawable.createFromStream(imageInputStream, null)
         val partImage = findViewById<ImageView>(R.id.pageImage)
         partImage.setImageDrawable(image)
-
-        //partImage.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
-        //partImage.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
     }
 
     // Set the current Chapter and Display Chapter contents
@@ -134,111 +128,82 @@ class BookScrollingActivity : AppCompatActivity() {
         storyChapter = storyChapter.replace("\\n", "\n\t\t\t")
 
         // Choice dialog
-        var option1: String
-        var option2: String
-        val nextPart = currentPart + 1
+        val options = mutableListOf<String>()
         val nextChoices = getNextChoices("$currentPart$currentChoice")
+        var choiceNumber = 1
 
-        if (nextChoices[0] == "END") {  // If the chapter is THE END, display chapter and "THE END".
-            option1 = ""
-            option2 = "THE END"
-        } else if (nextChoices[0].length == 2) {  // The chapter is not THE END, but has no choices.
-            option1 = ""
-            option2 = "Click to proceed"
-        } else {  // There are 2 choices. Display choice text
-            // Choice A text
-            var choice1Filter = "Choice.$currentPart$currentChoice.$nextPart"
-            choice1Filter += nextChoices[0]
-            val filteredOption1 = storyLines.filter { it.contains(choice1Filter) }
-            option1 = filteredOption1[0]
-            option1 = option1.substring(13)
-            option1 = "Choice 1: $option1"
-
-            // Choice B text
-            var choice2Filter = "Choice.$currentPart$currentChoice.$nextPart"
-            choice2Filter += nextChoices[1]
-            val filteredOption2 = storyLines.filter { it.contains(choice2Filter) }
-            option2 = filteredOption2[0]
-            option2 = option2.substring(13)
-            option2 = "Choice 2: $option2"
+        // If the chapter is THE END, display chapter and "THE END".
+        if (nextChoices[0] == "END") {
+            options.add("")
+            options.add("THE END")
+        }
+        // If the chapter is not THE END but has no choices, prompt to "proceed"
+        else if (nextChoices.size == 1 && nextChoices[0] != "END") {
+            options.add("")
+            options.add("Click to proceed")
+        }
+        // Otherwise, there are 2 choices or more choices. Display choice text.
+        if (nextChoices.size > 1) {
+            print("inside nextChoices > 1")
+            for (choice in nextChoices) {
+                val choiceFilter = "Choice.$currentPart$currentChoice.$choice"
+                val filteredOption = storyLines.filter { it.contains(choiceFilter) }
+                var optionString = filteredOption.toString()
+                optionString = optionString.substring(13)
+                optionString = "Choice $choiceNumber: $optionString"
+                optionString = optionString.dropLast(1)
+                options.add(optionString)
+                choiceNumber++
+            }
         }
 
-        // Display text
+        // Gather text strings for display
         val chapterDisplay = findViewById<TextView>(R.id.chapterContents)
-        val storyPage = SpannableString("$storyChapter\n\n$option1\n\n$option2")
-
-        // Italicize all strings "Seeker"
-        /*val seekerPattern = Pattern.compile("Seeker")
-        val seekerMatcher = seekerPattern.matcher(storyChapter)
-        while (seekerMatcher.find()) {
-            val seekerStringStart = seekerMatcher.start()
-            val seekerStringEnd = seekerMatcher.end()
-            storyPage.setSpan(
-                StyleSpan(Typeface.ITALIC), seekerStringStart, seekerStringEnd,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-        }*/
-
-        // Italicize all strings "Maray"
-        /*val marayPattern = Pattern.compile("Maray")
-        val marayMatcher = marayPattern.matcher(storyChapter)
-        while (marayMatcher.find()) {
-            val marayStringStart = marayMatcher.start()
-            val marayStringEnd = marayMatcher.end()
-            storyPage.setSpan(
-                StyleSpan(Typeface.ITALIC), marayStringStart, marayStringEnd,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-        }*/
-
-        // Italicize option1 line if there are 2 choices
-        if (option1 != "") {
-            val option1StartIndex = storyPage.indexOf(option1)
-            val option1EndIndex = option1StartIndex + option1.length
-            val option1Choice1EndIndex = option1StartIndex + "Choice 1:".length
-            val option1LineStartIndex = option1Choice1EndIndex + 1
-            storyPage.setSpan(
-                StyleSpan(Typeface.BOLD_ITALIC), option1StartIndex, option1Choice1EndIndex,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            storyPage.setSpan(
-                StyleSpan(Typeface.ITALIC), option1LineStartIndex, option1EndIndex,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
+        var storyPage = SpannableString("$storyChapter\n\n${options[0]}\n\n${options[1]}")
+        when (options.size) {
+            2 -> {
+                storyPage = SpannableString("$storyChapter\n\n${options[0]}\n\n${options[1]}")
+            }
+            3 -> {
+                storyPage = SpannableString("$storyChapter\n\n${options[0]}\n\n" +
+                        "${options[1]}\n\n${options[2]}")
+            }
+            4 -> {
+                storyPage = SpannableString("$storyChapter\n\n${options[0]}\n\n" +
+                        "${options[1]}\n\n${options[2]}\n\n${options[3]}")
+            }
         }
 
-        // Italicize option2 line if there are 2 choices
-        if (option2 != "THE END" && option2 != "Click to proceed") {
-            val option2StartIndex = storyPage.indexOf(option2)
-            val option2EndIndex = option2StartIndex + option2.length
-            val option2Choice2EndIndex = option2StartIndex + "Choice 2:".length
-            val option2LineStartIndex = option2Choice2EndIndex + 1
-            storyPage.setSpan(
-                StyleSpan(Typeface.BOLD_ITALIC), option2StartIndex, option2Choice2EndIndex,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            storyPage.setSpan(
-                StyleSpan(Typeface.ITALIC), option2LineStartIndex, option2EndIndex,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-        }
-
-        // Boldface option2 line if it is "THE END" or "Click to proceed"
-        else {
-            val option2StartIndex = storyPage.indexOf(option2)
-            val option2EndIndex = option2StartIndex + option2.length
-            storyPage.setSpan(
-                StyleSpan(Typeface.BOLD), option2StartIndex, option2EndIndex,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
+        // Text styling
+        for (i in 0 until options.size) {
+            // Italicize option lines if there are 2 or more choices
+            if (options[1] != "THE END" && options[1] != "Click to proceed") {
+                val optionStartIndex = storyPage.indexOf(options[i])
+                val optionEndIndex = optionStartIndex + options[i].length
+                val choiceValue = i + 1
+                val optionChoiceEndIndex = optionStartIndex + "Choice $choiceValue:".length
+                val optionLineStartIndex = optionChoiceEndIndex + 1
+                storyPage.setSpan(
+                    StyleSpan(Typeface.BOLD_ITALIC), optionStartIndex, optionChoiceEndIndex,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                storyPage.setSpan(
+                    StyleSpan(Typeface.ITALIC), optionLineStartIndex, optionEndIndex,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+            // Boldface option2 line if it is "THE END" or "Click to proceed"
+            else {
+                val theEndStartIndex = storyPage.indexOf(options[1])
+                val theEndEndIndex = theEndStartIndex + options[1].length
+                storyPage.setSpan(
+                    StyleSpan(Typeface.BOLD), theEndStartIndex, theEndEndIndex,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
         }
 
         chapterDisplay.text = storyPage
-
-        // Justify the TextView
-        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        //    chapterDisplay.justificationMode = Layout.JUSTIFICATION_MODE_INTER_WORD
-        //}
 
         // Scroll back to the top of the screen
         val storyView = findViewById<NestedScrollView>(R.id.storyLayout)
@@ -293,21 +258,53 @@ class BookScrollingActivity : AppCompatActivity() {
                 option2Btn.visibility = View.VISIBLE
                 option3Btn.visibility = View.GONE
 
-                // Left button proceeds to first choice
+                // Left button proceeds to first choice. Choices are 2 characters. Retain the second char.
                 option1Btn.setOnClickListener {
                     currentChoice = choiceList[0]
+                    currentChoice = currentChoice.substring(1)
                     storyDisplay()
                 }
 
                 // Right button proceeds to next choice
                 option2Btn.setOnClickListener {
                     currentChoice = choiceList[1]
+                    currentChoice = currentChoice.substring(1)
                     storyDisplay()
                 }
             }
 
             3 -> {
-                println("dummy2")
+                // Display left, right, and center option buttons
+                option1Btn.visibility = View.VISIBLE
+                option2Btn.visibility = View.VISIBLE
+                option3Btn.visibility = View.VISIBLE
+
+                // Rearrange image used for center button as "2" and right button as "3"
+                // Right button
+                option2Btn.setImageResource(R.drawable.ic_twotone_looks_3_24)
+                // Center button
+                option3Btn.setImageResource(R.drawable.ic_twotone_looks_two_24)
+
+                // Left button proceeds to choice 1. Choices are 2 characters. Retain the second char.
+                option1Btn.setOnClickListener {
+                    currentChoice = choiceList[0]
+                    currentChoice = currentChoice.substring(1)
+                    storyDisplay()
+                }
+
+                // Right button proceeds to choice 3
+                option2Btn.setOnClickListener {
+                    currentChoice = choiceList[2]
+                    currentChoice = currentChoice.substring(1)
+                    storyDisplay()
+                }
+
+                // Center button proceeds to next choice
+                option3Btn.setOnClickListener {
+                    currentChoice = choiceList[1]
+                    currentChoice = currentChoice.substring(1)
+                    storyDisplay()
+                }
             }
         }
     }
